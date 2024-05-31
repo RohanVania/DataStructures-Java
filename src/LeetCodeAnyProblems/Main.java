613adb19a0fa92dabe76ccaf761f9418fa054037
rohan <rohan@claclaws.com>
2024-05-31 18:57:13 -0400

    'LeetCodeProblems'

---
+++ src/LeetCodeAnyProblems/Main.java
@@ -0,0 +1,23 @@
+package LeetCodeAnyProblems;
+
+/*
+ * 1. Merge Two Sorted Array 
+ * 
+ *
+ */
+
+class Problems{
+	public static void mergeSortedArray(int []ar1,int []ar2) {
+		int ar1length=ar1.length;
+		
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
